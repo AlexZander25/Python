f4ed04c194de47c2aa82a332ea4122c666166156
Alexander <alexander.manuel.arce.gonzalez@gmail.com>
2024-01-17 22:22:44 -0300

    Operadores

---
+++ 02_operators.py
@@ -0,0 +1,44 @@
+### Operadores aritmeticos ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 //4)
+
+print("Hola " + "Python " + "Que tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+### Operadores comparativos ###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa") # Ordenacion alfabetica por ascii
+print(len("aaaa") >= len("abaa")) # cuenta caracteres
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+
+### Operadores logicos ###
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not 3 > 4)
